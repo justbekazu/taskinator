67f59166fb8e9de6531a7dfc3d9bc0e0b1c9740c
djkaz <justbekazu@gmail.com>
2021-03-07 21:14:06 -0600

    added lines

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li"); 
+    listItemEl.className = "task-item";
+    listItemEl.textConten = "this is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+
+buttonEl.addEventListener("click", createTaskHandler); 
+
+
